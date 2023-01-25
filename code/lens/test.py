9a619b664d6be29e2b9645c8ac465ee5a2499788
Jacob Cunningham <jcun4128@gmail.com>
2023-01-24 21:15:39 -0600

    test code

---
+++ code/lens/test.py
@@ -0,0 +1,21 @@
+# https://www.teachmemicro.com/raspberry-pi-pwm-servo-tutorial/
+
+import pigpio
+import time
+
+pi = pigpio.pi()
+focus_servo = 12
+focus_pos = 500
+
+tele_servo = 13
+tele_pos = 500
+
+def min_max():
+  pi.set_servo_pulsewidth(focus_servo, 500)
+  time.sleep(5)
+
+  for pw in range(2500,500,-1):
+    pi.set_servo_pulsewidth(focus_servo, pw)
+    time.sleep(0.001)
+
+min_max()
