9d12f8a0a388e06d7d6fbb6bf39bc9c3f2e54367
Jacob Cunningham <jcun4128@gmail.com>
2023-01-27 23:16:51 -0600

    some calibration

--- code/lens/test_stepper.py
+++ code/lens/test_stepper.py
@@ -1,15 +1,16 @@
 import time as og_time
 from stepper_motion import *
-from steper_motion_2 import *
 
 init_gpio_pins()
 
 def rotate(steps):
   stepper_clockwise(steps)
+  # stepper_counter_clockwise(steps)
 
 og_time.sleep(5)
 
-test(50)
+rotate(10)
 
 
 # 350 focus ring
+# 300 tele
