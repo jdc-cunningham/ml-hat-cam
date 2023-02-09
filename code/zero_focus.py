d71fce32e033662a79274d52b8dce820391bc000
Jacob Cunningham <jcun4128@gmail.com>
2023-02-09 00:24:42 -0600

    minor org

--- code/zero_focus.py
+++
@@ -1,7 +0,0 @@
-import time as og_time
-
-from stepper.stepper import Stepper
-
-focus_ring = Stepper(6, 13, 19, 26, 'focus', 350)
-
-focus_ring.zero_stepper()
--- code/zero_tele.py
+++
@@ -1,7 +0,0 @@
-import time as og_time
-
-from stepper.stepper import Stepper
-
-tele_ring = Stepper(12, 16, 20, 21, 'tele', 300)
-
-tele_ring.zero_stepper()
