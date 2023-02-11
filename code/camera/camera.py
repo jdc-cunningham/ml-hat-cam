f26601af1ac5cfe7ff4b742250f5aa21eaad9562
Jacob Cunningham <jcun4128@gmail.com>
2023-02-11 15:36:03 -0600

    doubt this will work

--- code/camera/camera.py
+++ code/camera/camera.py
@@ -7,12 +7,16 @@
 import io
 import logging
 import socketserver
+import cv2 as cv
+import numpy as np
+
 from http import server
 from threading import Condition, Thread
-
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
+
+output = None
 
 PAGE = """\
 <html>
@@ -25,75 +29,90 @@ PAGE = """\
 </html>
 """
 
-
 class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = Condition()
+  def __init__(self):
+    self.frame = None
+    self.condition = Condition()
 
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
+  def write(self, buf):
+    with self.condition:
+      self.frame = buf
+      self.condition.notify_all()
 
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+  def __init__(self, focus_ring, tele_ring):
+    self.focus_ring = focus_ring
+    self.tele_ring = tele_ring
+    self.stream_count = 0
+
+  def get_variance(frame_buffer):
+    img = cv.imdecode(frame_buffer, cv.IMREAD_COLOR)
+    return cv.Laplacian(img, cv.CV_64F).var()
+
+  def do_GET(self):
+    if self.path == '/':
+      self.send_response(301)
+      self.send_header('Location', '/index.html')
+      self.end_headers()
+    elif self.path == '/index.html':
+      content = PAGE.encode('utf-8')
+      self.send_response(200)
+      self.send_header('Content-Type', 'text/html')
+      self.send_header('Content-Length', len(content))
+      self.end_headers()
+      self.wfile.write(content)
+    elif self.path == '/stream.mjpg':
+      self.send_response(200)
+      self.send_header('Age', 0)
+      self.send_header('Cache-Control', 'no-cache, private')
+      self.send_header('Pragma', 'no-cache')
+      self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+      self.end_headers()
+
+      try:
+        while True:
+          with output.condition:
+            output.condition.wait()
+            frame = output.frame
+
+          self.wfile.write(b'--FRAME\r\n')
+          self.send_header('Content-Type', 'image/jpeg')
+          self.send_header('Content-Length', len(frame))
+          self.end_headers()
+          self.wfile.write(frame)
+          self.wfile.write(b'\r\n')
+
+          self.stream_count += 1
+
+          if (self.stream_count % 2 == 0):
+            sample_img = np.fromstring(frame, np.uint8)
+            print(self.get_variance(frame))
+
+      except Exception as e:
+        logging.warning(
+          'Removed streaming client %s: %s',
+          self.client_address, str(e))
+    else:
+      self.send_error(404)
+      self.end_headers()
 
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-output = None
-
-def start_web_stream():
+def start_web_stream(focus_ring, tele_ring):
   global output
 
   picam2 = Picamera2()
-  picam2.configure(picam2.create_video_configuration(main={"size": (1024, 720)}))
+  picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
   output = StreamingOutput()
   picam2.start_recording(JpegEncoder(), FileOutput(output))
 
   try:
-      address = ('', 8000)
-      server = StreamingServer(address, StreamingHandler)
-      server.serve_forever()
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler(focus_ring, tele_ring))
+    server.serve_forever()
   finally:
-      picam2.stop_recording()
+    picam2.stop_recording()
--- code/main.py
+++ code/main.py
@@ -1,7 +1,10 @@
 from database.database import Database
 from lens_stepper.stepper import Stepper
+from camera import start_web_stream
 
 db = Database()
 
 focus_ring = Stepper(6, 13, 19, 26, 'focus', 350, db)
 tele_ring = Stepper(12, 16, 20, 21, 'tele', 300, db)
+
+start_web_stream(focus_ring, tele_ring)
