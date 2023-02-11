88d9d4bbd0187f1d67b53e949fdbf14c05ba5f3c
Jacob Cunningham <jcun4128@gmail.com>
2023-02-11 13:39:27 -0600

    nesting classes or make it work

--- code/camera/camera.py
+++ code/camera/camera.py
@@ -21,68 +21,67 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
-camera = None
+class Camera:
+  camera = None
 
-class StreamingOutput(io.BufferedIOBase):
-  def __init__(self):
-    self.frame = None
-    self.condition = Condition()
+  class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+      self.frame = None
+      self.condition = Condition()
 
-  def write(self, buf):
-    with self.condition:
-      self.frame = buf
-      self.condition.notify_all()
+    def write(self, buf):
+      with self.condition:
+        self.frame = buf
+        self.condition.notify_all()
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-  def do_GET(self):
-    output = camera.output
+  class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+      output = camera.output
 
-    if self.path == '/':
-      self.send_response(301)
-      self.send_header('Location', '/index.html')
-      self.end_headers()
-    elif self.path == '/index.html':
-      content = camera.page.encode('utf-8')
-      self.send_response(200)
-      self.send_header('Content-Type', 'text/html')
-      self.send_header('Content-Length', len(content))
-      self.end_headers()
-      self.wfile.write(content)
-    elif self.path == '/stream.mjpg':
-      self.send_response(200)
-      self.send_header('Age', 0)
-      self.send_header('Cache-Control', 'no-cache, private')
-      self.send_header('Pragma', 'no-cache')
-      self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-      self.end_headers()
+      if self.path == '/':
+        self.send_response(301)
+        self.send_header('Location', '/index.html')
+        self.end_headers()
+      elif self.path == '/index.html':
+        content = camera.page.encode('utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(content))
+        self.end_headers()
+        self.wfile.write(content)
+      elif self.path == '/stream.mjpg':
+        self.send_response(200)
+        self.send_header('Age', 0)
+        self.send_header('Cache-Control', 'no-cache, private')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+        self.end_headers()
 
-      try:
-        while True:
-          with output.condition:
-            output.condition.wait()
-            frame = output.frame
-          self.wfile.write(b'--FRAME\r\n')
-          self.send_header('Content-Type', 'image/jpeg')
-          self.send_header('Content-Length', len(frame))
-          self.end_headers()
-          self.wfile.write(frame)
-          self.wfile.write(b'\r\n')
-          
-      except Exception as e:
+        try:
+          while True:
+            with output.condition:
+              output.condition.wait()
+              frame = output.frame
+            self.wfile.write(b'--FRAME\r\n')
+            self.send_header('Content-Type', 'image/jpeg')
+            self.send_header('Content-Length', len(frame))
+            self.end_headers()
+            self.wfile.write(frame)
+            self.wfile.write(b'\r\n')
+            
+        except Exception as e:
           logging.warning(
-              'Removed streaming client %s: %s',
-              self.client_address, str(e))
-    else:
+            'Removed streaming client %s: %s',
+            self.client_address, str(e))
+      else:
         self.send_error(404)
         self.end_headers()
 
+  class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+      allow_reuse_address = True
+      daemon_threads = True
 
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-class Camera:
-  def __init__(self, stepper):
+  def __init__(self): # stepper
     self.stream_to_web = False
     self.web_stream_page = ""
     self.resolution = (1024, 720) # tuple
@@ -109,16 +108,16 @@ class Camera:
     picam2 = Picamera2()
     # picam2.configure(picam2.create_video_configuration(main={"size": self.resolution}))
     picam2.configure(picam2.create_video_configuration(main={"size": self.resolution}))
-    output = StreamingOutput()
+    output = self.StreamingOutput()
     self.output = output
     picam2.start_recording(JpegEncoder(), FileOutput(output))
 
     try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler())
-        server.serve_forever()
+      address = ('', 8000)
+      server = self.StreamingServer(address, self.StreamingHandler())
+      server.serve_forever()
     finally:
-        picam2.stop_recording()
+      picam2.stop_recording()
 
 camera = Camera()
 camera.start_web_stream()
