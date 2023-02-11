39676d5a3962058e70787113e2651b8c87b64661
Jacob Cunningham <jcun4128@gmail.com>
2023-02-11 16:44:43 -0600

    oof

--- code/main.py
+++ code/main.py
@@ -1,10 +1,11 @@
 from database.database import Database
 from lens_stepper.stepper import Stepper
-from camera.camera import start_web_stream
 
 db = Database()
 
 focus_ring = Stepper(6, 13, 19, 26, 'focus', 350, db)
 tele_ring = Stepper(12, 16, 20, 21, 'tele', 300, db)
+
+from camera.camera import start_web_stream
 
 start_web_stream()
