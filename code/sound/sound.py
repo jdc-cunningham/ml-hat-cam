e2e23eb11d80c3b26d27d52c4295081b63bea56c
Jacob Cunningham <jcun4128@gmail.com>
2023-02-18 17:01:16 -0600

    sound state

--- code/sound/sound.py
+++ code/sound/sound.py
@@ -7,8 +7,11 @@ class Sound:
     self.playing_sound = False
 
   def play_sound_file(self, sound_file):
+    self.playing_soudn = True
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
     
     while pygame.mixer.music.get_busy() == True:
       continue
+
+    self.playing_sound = False
