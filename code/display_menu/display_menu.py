b09f66fc42c98e92f5f28f2361d0389e36176e75
Jacob Cunningham <jcun4128@gmail.com>
2023-02-18 18:50:30 -0600

    working class oled calls

--- code/display_menu/display_menu.py
+++ code/display_menu/display_menu.py
@@ -1,18 +1,16 @@
+# this file is meant to be imported/used externally
+# due to the directory imports
+
 import sys
 import os
-
-picdir = os.getcwd() + '/pic'
-libdir = os.getcwd() + '/lib'
-
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
-import logging    
+import logging
 import time
 import traceback
-from waveshare_OLED import OLED_1in5_rgb
-from PIL import Image,ImageDraw,ImageFont
-logging.basicConfig(level=logging.DEBUG)
+
+from lib.waveshare_OLED import OLED_1in5_rgb
+from PIL import Image, ImageDraw, ImageFont
+
+base_dir = os.getcwd()
 
 colors = {
   'red': 'RED',
@@ -26,22 +24,24 @@ colors = {
 }
 
 fonts = {
-  'font_1': ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 12),
-  'font_2': ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18),
-  'font_3': ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+  'font_1': ImageFont.truetype(base_dir + '/display_menu/pic/Font.ttc', 12),
+  'font_2': ImageFont.truetype(base_dir + '/display_menu/pic/Font.ttc', 18),
+  'font_3': ImageFont.truetype(base_dir + '/display_menu/pic/Font.ttc', 24)
 }
 
 class DisplayMenu():
   def __init__(self):
-    disp = OLED_1in5_rgb.OLED_1in5_rgb()
-    disp.Init()
-    disp.clear()
-    self.text_color = colors.white
+    self.disp = OLED_1in5_rgb.OLED_1in5_rgb()
+    self.disp.Init()
+    self.disp.clear()
+    self.image = Image.new('RGB', (self.disp.width, self.disp.height), 0)
+    self.draw = ImageDraw.Draw(self.image)
+    self.text_color = colors['white']
 
-  def draw_text(self, x, y, text, font = fonts.font_1, color = self.text_color):
-    draw.text((x,y), text, font, color)
-    disp.ShowImage(disp.getbuffer(image1))
+  def draw_text(self, x, y, text, font = fonts['font_2'], color = colors['white']):
+    self.draw.text((x,y), text, font = font, fill = color)
+    self.disp.ShowImage(self.disp.getbuffer(self.image))
 
-  def draw_line(self, start_coord, end_coord, color = self.text_color):
-    draw.line([(start_coord[0],start_coord[1]),(end_coord[0],end_coord[1])], color)
-    disp.ShowImage(disp.getbuffer(image1))
+  def draw_line(self, start_coord, end_coord, fill = colors['white']):
+    self.draw.line([(start_coord[0],start_coord[1]),(end_coord[0],end_coord[1])], color)
+    self.disp.ShowImage(self.disp.getbuffer(self.image))
