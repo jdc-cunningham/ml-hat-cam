a75bea739ff94778665ea29815505b9c542b6f2a
Jacob Cunningham <jcun4128@gmail.com>
2023-02-25 14:45:47 -0600

    unindent issue

--- code/speech_to_intent/speech_to_intent.py
+++ code/speech_to_intent/speech_to_intent.py
@@ -14,19 +14,25 @@ class SpeechIntent():
 
 	def start_listening(self):
 		self.active = True
-		picovoice_ai_key = os.environ.get(
-			'picovoice_ai_access_key', os.getcwd())
-		cmd_string = 'rhino_demo_mic --access_key ' + picovoice_ai_key + \
-			' --context_path Zooming_en_raspberry-pi_v2_1_0.rhn --audio_device_index 1'
+		picovoice_ai_key = os.environ.get('picovoice_ai_access_key', os.getcwd())
+		cmd_string = 'rhino_demo_mic --access_key ' + picovoice_ai_key + ' --context_path Zooming_en_raspberry-pi_v2_1_0.rhn --audio_device_index 1'
 		args = (sys.executable, '-u', cmd_string)
 		cmd = ' '.join(args)
-		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
-			stderr=STDOUT, universal_newlines=True)
+		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
 
-		while (self.active):
-			lines = (p.stdout.readline, '')
-			print(lines)
-			line = lines[len(lines) - 1]
-			line = line.rstrip()
-			print(line.rstrip())
-			time.sleep(0.5)
+    lines = iter(p.stdout.readline, '')
+    for line in lines:
+      line = line.rstrip()
+      print(line)
+
+		# while (self.active):
+			# lines = (p.stdout.readline, '')
+			# print(lines)
+			# line = lines[len(lines) - 1]
+			# line = line.rstrip()
+			# print(line.rstrip())
+			# time.sleep(0.5)
+
+    # for line in iter(p.stdout.readline,''):
+      # line = line.rstrip()
+      # print(line)
