f62d39009eaac0429f50a3dd07d72a4da6fb373e
Jacob Cunningham <jcun4128@gmail.com>
2023-02-25 14:35:00 -0600

    why did this indent change

--- code/speech_to_intent/speech_to_intent.py
+++ code/speech_to_intent/speech_to_intent.py
@@ -1,7 +1,10 @@
-import os, sys, time
+import os
+import sys
+import time
 from subprocess import Popen, PIPE, STDOUT
 
 # https://stackoverflow.com/questions/51133407/capture-stdout-and-stderr-of-process-that-runs-an-infinite-loop
+
 
 class SpeechIntent():
 	def __init__(self, zoom_in_fcn, zoom_out_fcn):
@@ -11,15 +14,18 @@ class SpeechIntent():
 
 	def start_listening(self):
 		self.active = True
-		picovoice_ai_key = os.environ.get('picovoice_ai_access_key', os.getcwd())
-		cmd_string = 'rhino_demo_mic --access_key ' + picovoice_ai_key + ' --context_path Zooming_en_raspberry-pi_v2_1_0.rhn --audio_device_index 1'
+		picovoice_ai_key = os.environ.get(
+			'picovoice_ai_access_key', os.getcwd())
+		cmd_string = 'rhino_demo_mic --access_key ' + picovoice_ai_key + \
+			' --context_path Zooming_en_raspberry-pi_v2_1_0.rhn --audio_device_index 1'
 		args = (sys.executable, '-u', cmd_string)
 		cmd = ' '.join(args)
-		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
+		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
+			stderr=STDOUT, universal_newlines=True)
 
 		while (self.active):
-                  lines = (p.stdout.readline,'')
-                  line = lines[len(lines) -1]
-                  line = line.rstrip()
-	          print(line.rstrip())
-	          time.sleep(0.5)
+			lines = (p.stdout.readline, '')
+			line = lines[len(lines) - 1]
+			line = line.rstrip()
+			print(line.rstrip())
+			time.sleep(0.5)
