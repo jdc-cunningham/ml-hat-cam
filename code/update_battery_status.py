9a5963cc5ff296df9eba293f81143142e951c43d
Jacob Cunningham <jcun4128@gmail.com>
2023-02-24 19:30:28 -0600

    only update db

--- code/update_battery_status.py
+++ code/update_battery_status.py
@@ -3,20 +3,7 @@
 # shouldn't let it get that high that's absolutely exhausted where battery protection
 # kicks in for my particular batteries
 
-import os, sys
-
-lib_dir = os.getcwd() + '/display_menu'
-sys.path.append(lib_dir)
-
-from display_menu.display_menu import DisplayMenu
 from batt_db.batt_db import BattDatabase
 
 batt_db = BattDatabase()
 batt_db.update_batt_uptime()
-dmenu = DisplayMenu()
-dmenu.init()
-
-batt_status = batt_db.get_batt_status()
-dmenu.draw_text(0, 0, 'batt: ' + batt_status, 'font_1', 'WHITE')
-
-# hmm... need long-running service access even from cron/systemd
