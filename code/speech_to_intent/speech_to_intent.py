bdaee9db3919acb5d94c0da36650994e16052e80
Jacob Cunningham <jcun4128@gmail.com>
2023-02-25 14:56:18 -0600

    not working

--- code/speech_to_intent/speech_to_intent.py
+++ code/speech_to_intent/speech_to_intent.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 import subprocess
-# from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT
 
 # https://stackoverflow.com/questions/51133407/capture-stdout-and-stderr-of-process-that-runs-an-infinite-loop
 # https://stackoverflow.com/questions/803265/getting-realtime-output-using-subprocess
@@ -17,10 +17,9 @@ class SpeechIntent():
 		self.active = True
 		picovoice_ai_key = os.environ.get('picovoice_ai_access_key', os.getcwd())
 		cmd = 'picovoice_ai_rhino_mic_demo.py --access_key ' + picovoice_ai_key + ' --context_path Zooming_en_raspberry-pi_v2_1_0.rhn --audio_device_index 1'
-		p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1)
+		p = Popen('svnadmin verify /var/svn/repos/config', stdout = PIPE, stderr = STDOUT, shell = True)
 		
-		for line in iter(p.stdout.readline, b''):
+		while self.active:
+			line = p.stdout.readline()
 			print(line)
-			
-		p.stdout.close()
-		p.wait()
+			time.sleep(0.5)
