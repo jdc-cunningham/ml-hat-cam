4bf22b5760edf43e38374c8ac3903746b177cf54
Jacob Cunningham <jcun4128@gmail.com>
2023-02-25 15:40:13 -0600

    working

--- code/test_speech_intent.py
+++ code/test_speech_intent.py
@@ -1,4 +1,8 @@
-from speech_to_intent.speech_to_intent import SpeechIntent
+import os, pvrhino
+from dotenv import load_dotenv
+from speech_to_intent.speech_to_intent import RhinoDemo
+
+load_dotenv("/home/pi/ml-hat-cam/code/speech_to_intent/.env")
 
 def zoom_in():
   print("zoom in")
@@ -6,5 +10,19 @@ def zoom_in():
 def zoom_out():
   print("zoom out")
 
-speech = SpeechIntent(zoom_in, zoom_out)
-speech.start_listening()
+def parse_output(output):
+  print(output)
+
+picovoice_ai_key = os.getenv('PV_AI_ACCESS_KEY')
+
+speechIntent = RhinoDemo(
+  access_key=picovoice_ai_key,
+  library_path=pvrhino.LIBRARY_PATH,
+  model_path=pvrhino.MODEL_PATH,
+  context_path=os.getcwd() + '/speech_to_intent/Zooming_en_raspberry-pi_v2_1_0.rhn',
+  endpoint_duration_sec=1,
+  require_endpoint=False,
+  audio_device_index=1,
+  output_path=None,
+  output_callback=parse_output
+).run()
