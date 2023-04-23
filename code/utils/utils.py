9980aed9d47867678631b14c4ba37e964dc69a00
Jacob Cunningham <jcun4128@gmail.com>
2023-04-23 04:37:42 -0500

    note to self

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -5,6 +5,6 @@ class Utils:
   def __init__(self):
     self.ip = '000.000.000.000'
 
-  def get_ip():
+  def get_ip(self):
     hostname = socket.gethostname()
     return socket.gethostbyname(hostname)
