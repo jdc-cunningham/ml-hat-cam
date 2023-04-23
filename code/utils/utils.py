6716e9087eb7134a9873b244e2d7c29566c444ce
Jacob Cunningham <jcun4128@gmail.com>
2023-04-23 04:52:24 -0500

    nasty way to get local ip

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,11 +1,12 @@
-# https://www.geeksforgeeks.org/python-program-find-ip-address
-# https://stackoverflow.com/a/57355707/2710227
-import socket
+import os
 
 class Utils:
   def __init__(self):
     self.ip = '000.000.000.000'
 
+  # https://stackoverflow.com/a/3503909/2710227
   def get_ip(self):
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname + ".local")
+    ifconfig_out = os.popen('ifconifg').read()
+    local_ip = '192' + ifconfig_out.split('inet 192')[1].split('  netmask')[0]
+
+    return local_ip or 'no ip' # can happen not on wifi
