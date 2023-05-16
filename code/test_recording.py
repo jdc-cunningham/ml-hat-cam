1fcd69fa2641ccbe671e74923a5f6cfb73c7bbd7
Jacob Cunningham <jcun4128@gmail.com>
2023-05-16 08:29:26 -0500

    starting recording work

---
+++ code/test_recording.py
@@ -0,0 +1,8 @@
+import picamera2 from Picamera2
+
+camera = picamera2()
+
+camera.resolution = (640, 480)
+camera.start_recording('/dev/sda/my_video.h264')
+camera.wait_recording(60)
+camera.stop_recording()
