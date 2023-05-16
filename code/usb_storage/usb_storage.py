6194978d59b8b37a1b7ddc4d6d596b1b68bbd67a
Jacob Cunningham <jcun4128@gmail.com>
2023-05-16 14:19:31 -0500

    hmm

--- code/usb_storage/usb_storage.py
+++ code/usb_storage/usb_storage.py
@@ -6,4 +6,9 @@ class UsbStorage():
 
   def check_mounted(self):
      # sdb1 confirmed beforehand with lsblk
-    self.mounted = subprocess.check_output('mount /dev/sdb1 /mnt', shell=True) == 0
+    trya = subprocess.check_output('mount /dev/sda1 /mnt', shell=True) == 0
+
+    if (trya != 0):
+      tryb = subprocess.check_output('mount /dev/sda1 /mnt', shell=True) == 0
+
+    self.mounted = trya == 0 or tryb == 0 # what
