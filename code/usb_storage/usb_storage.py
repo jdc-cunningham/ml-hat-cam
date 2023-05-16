8effb15cc10f8b4f1d6da4da57c564e875decd29
Jacob Cunningham <jcun4128@gmail.com>
2023-05-16 14:24:17 -0500

    neat

--- code/usb_storage/usb_storage.py
+++ code/usb_storage/usb_storage.py
@@ -1,10 +1,16 @@
+# https://www.geeksforgeeks.org/python-os-path-ismount-method/#
+import os
 import subprocess
 
 class UsbStorage():
   def __init__(self):
     self.mounted = False
+    self.mount_path = '/mnt'
 
   def check_mounted(self):
+    if (os.path.ismount(self.mount_path)):
+      return True # not necessarily the right device, but code below assigns it
+
      # sdb1 confirmed beforehand with lsblk
     trya = subprocess.check_output('mount /dev/sda1 /mnt', shell=True) == 0
 
