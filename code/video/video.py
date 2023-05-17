1d0dc6fa9ca62ef10269ec1438f759729afb44be
Jacob Cunningham <jcun4128@gmail.com>
2023-05-16 23:14:06 -0500

    wth is this

--- code/video/video.py
+++ code/video/video.py
@@ -1,4 +1,4 @@
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import H264Encoder, Quality
 from picamera2 import Picamera2
 import time
 
@@ -10,11 +10,11 @@ class Video:
     self.record_path = usb_path
     # 1080P@60fps
     # 4056, 3040 max lower fps, possibly not possible with rpi
-    self.camera.create_video_configuration(main={"size": (1920, 1080)})
+    self.camera.create_video_configuration()
 
   def start_recording(self, file_name):
     self.filename = file_name
-    self.camera.start_recording(self.encoder, self.record_path + self.filename + '.h264')
+    self.camera.start_recording(self.encoder, self.record_path + self.filename + '.h264', Quality.HIGH)
 
   def stop_recording(self):
     self.camera.stop_recording()
