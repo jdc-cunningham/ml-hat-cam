ff5d82e5af3b2711e6eb5f8f534f7ec33a818412
Jacob Cunningham <jcun4128@gmail.com>
2023-06-08 21:34:45 -0500

    don't gots it

--- code/video/tmp_video.py
+++ code/video/tmp_video.py
@@ -17,8 +17,7 @@ class Video:
     # 1080P@60fps
     # 4056, 3040 max lower fps, possibly not possible with rpi
     main_stream_res = {"size": (1280, 720)}
-    sample_stream_res = {"size": (640, 480)}
-    vid_config = self.camera.create_video_configuration(main_stream_res, sample_stream_res, encode="lores")
+    vid_config = self.camera.create_video_configuration(main_stream_res)
     self.camera.configure(vid_config)
 
   def start_recording(self, file_name):
@@ -31,6 +30,7 @@ class Video:
     # https://stackoverflow.com/a/32264327/2710227
     pil_img = im.fromarray(np.uint8(np_arr))
     cv_img = cv.cvtColor(np.array(pil_img), cv.COLOR_RGB2BGR)
+    cv.imwrite(str(int(time.time())) + '.jpg', cv_img)
     var = round(cv.Laplacian(cv_img, cv.CV_64F).var(), 2)
     return var
 
@@ -42,17 +42,17 @@ class Video:
       focus_far = True # first dir
 
       if (self.pause_autofocus != True):
-        np_arr = self.camera.capture_array("lores")
+        np_arr = self.camera.capture_array("main")
         variance = self.get_variance(np_arr)
         
         if (prev_var == 0):
           prev_var = variance
-          self.focus_stepper.focus_far(25)
+          self.focus_stepper.focus_far(5)
         else:
-          if (variance > prev_var or variance < 100):
-            self.focus_stepper.focus_far(25)
+          if (variance < prev_var or variance < 50):
+            self.focus_stepper.focus_far(5)
           else:
-            self.focus_stepper.focus_near(25)
+            self.focus_stepper.focus_near(5)
 
       print(prev_var)
 
